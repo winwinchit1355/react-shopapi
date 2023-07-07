f1beff5013d7d94c023acadb0d97ad1c36f7104c
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2023-07-07 11:32:20 +0630

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MetalApiController;
+use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Api\CategoryApiController;
+use App\Http\Controllers\Api\GemstoneApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('/categories', CategoryApiController::class);
+Route::resource('/metals', MetalApiController::class);
+Route::resource('/gemstones', GemstoneApiController::class);
+Route::resource('/products', ProductApiController::class);
+Route::get('/shop', [ProductApiController::class, "shop"]);
+Route::get('/product-detail/{slug}', [ProductApiController::class, "productDetail"]);
